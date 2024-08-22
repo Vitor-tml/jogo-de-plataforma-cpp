cd7121ff716bbb88511121e8820e5b8837cc2585
GustavoPellanda <gusfipe@gmail.com>
2024-08-22 19:04:36 -0300

    Trabalhando no inimigo. Movimentação bugadassa.

--- src/game/Character/Enemy/Enemy.cpp
+++ src/game/Character/Enemy/Enemy.cpp
@@ -20,7 +20,25 @@ void Enemy::inimigoTeste(Renderer& renderer) {
 
 bool indoDireita = true;
 void Enemy::mover(float deltaTime) {
+    static float velocidadeHorizontal = 100.0f; // Pixels por segundo
+    static float distancia = 200.0f; // Distância total do movimento
+    static float posXInicial = shape.getPosition().x;
 
+    float limiteDireita = posXInicial + distancia;
+    float limiteEsquerda = posXInicial - distancia;
+
+    if (indoDireita) {
+        velocidadeHorizontal += 200.0f;
+        shape.move(velocidadeHorizontal * deltaTime, 0.0f);
+        if (shape.getPosition().x >= limiteDireita) {
+            indoDireita = false;
+        }
+    } else {
+        shape.move(-velocidadeHorizontal * deltaTime, 0.0f);
+        if (shape.getPosition().x <= limiteEsquerda) {
+            indoDireita = true;
+        }
+    }
 }
 
 void Enemy::executar(float deltaTime)
